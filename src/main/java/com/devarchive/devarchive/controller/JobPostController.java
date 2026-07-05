01d77c266acf701afd24a1239af4cbc9a4d76cd6
ksh0825-ato <bey0825@gmail.com>
2026-07-05 13:37:05 +0900

    ui 수정(구인 공고 리스트, 상세 보기 수정)

--- src/main/java/com/devarchive/devarchive/controller/JobPostController.java
+++ src/main/java/com/devarchive/devarchive/controller/JobPostController.java
@@ -1,6 +1,7 @@
 package com.devarchive.devarchive.controller;
 
 import java.security.Principal;
+import java.time.LocalDate;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.devarchive.devarchive.domain.JobPost;
 import com.devarchive.devarchive.dto.jobpost.JobPostDto;
+import com.devarchive.devarchive.repository.JobPostRepository;
 import com.devarchive.devarchive.service.JobPostService;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -27,22 +29,20 @@ import lombok.RequiredArgsConstructor;
 public class JobPostController {
     // 1. private final로 선언
     private final JobPostService jobPostService;
+    private final JobPostRepository jobPostRepository;
 
     // 공고 목록 조회
     @GetMapping("/JobPostList")
     public String jobPostList(Model model, HttpServletRequest request,@PageableDefault(size = 10) Pageable pageable) {
         System.out.println("게시판 조회 진입 시도");
+            Page<JobPost> jobPostList = jobPostRepository.findAll(pageable);
 
-        // 세션 검증 로직 제거! 
-        // Spring Security가 설정된 권한(SecurityConfig)에 따라 
-        // 인증되지 않은 사용자는 아예 이 메서드에 들어오지 못하게 차단합니다.
-    
-        Page<JobPost> jobPostList = jobPostService.getJobPostList(pageable);
+            model.addAttribute("job", jobPostList);
+            model.addAttribute("maxPage", 5);
+            // 오늘 날짜를 모델에 추가 (마감일 계산용)
+            model.addAttribute("today", java.time.LocalDate.now()); 
 
-        model.addAttribute("job", jobPostList);
-        model.addAttribute("maxPage", 5);
-
-        return "job/JobPostList";
+            return "job/JobPostList";
 
     }
 
@@ -56,10 +56,14 @@ public class JobPostController {
     // 게시글 등록 처리
     @PreAuthorize("hasRole('COMPANY')")
     @PostMapping("/JobPostRegister")
-    public String jobPostRegister(@ModelAttribute JobPostDto jobPostDto, Principal principal) {
+    public String jobPostRegister(@ModelAttribute JobPostDto jobPostDto, Model model, Principal principal, Pageable pageable) {
+        Page<JobPost> jobPosts = jobPostService.getJobPostList(pageable);
 
         // 1. 데이터 확인을 위한 로그 (값이 잘 넘어오는지 확인)
         System.out.println("등록 시도 - 제목: " + jobPostDto.getJobPostTitle());
+
+        model.addAttribute("today", LocalDate.now());
+        model.addAttribute("job", jobPosts);
         
         // 2. 서비스 호출 (username을 principal.getName()으로 전달)
         // 이 부분이 빠져 있으면 아무것도 저장되지 않습니다.
